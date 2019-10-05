8b3d8a371f9b99724ae8287f0bdc3742300a90b2
Jeroen <Jeroenbruinsma@gmail.com>
2019-10-05 11:26:50 +0200

    install cors

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require("express");
 const Sequelize = require("sequelize");
 const bodyParser = require('body-parser')
 const Sse = require('json-sse')
+const cors = require('cors')
+
+
 
 const stream = new Sse()
 
 const databaseUrl =
   process.env.DATABASE_URL ||
   "postgres://postgres:secret@localhost:5432/postgres";
-  
+
+
 const jsonParser = bodyParser.json()
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors())
 app.use(jsonParser)
 
 app.listen(port, () => console.log("listning on port ", port));
@@ -53,11 +59,8 @@ app.get('/stream', async (request, response) => {
     console.log('got a request on stream')
     const room = await Chatroom.findAll()
     const data = JSON.stringify(room)
-    console.log('messages in this room are',data)
+    //console.log('messages in this room are',data)
 
     stream.updateInit(data)
     stream.init(request,response)
-
-    
-
 })
